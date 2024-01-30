c4fbd69de04c8d4ed6a659107c853528dc6d6df0
Sunja-An <fellin1919@naver.com>
2024-01-30 19:52:34 +0900

    Feat: File structure added

---
+++ src/pages/Errorpage/Errorpage.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Errorpage() {
+  return (
+    <div>Errorpage</div>
+  )
+}
---
+++ src/pages/Layoutpage/Layoutpage.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Layoutpage() {
+  return (
+    <div>Layoutpage</div>
+  )
+}
